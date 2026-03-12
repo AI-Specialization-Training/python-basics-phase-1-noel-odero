4a818399ef857235787b455c8ebc1f7b5f14a030
Noel Odero <anoel.odero@gmail.com>
2026-03-12 19:55:53 +0200

    Refactor calc function to include all specified operations

--- exercise-3.py
+++ exercise-3.py
@@ -7,3 +7,23 @@
 # a string that is one of the following `add`, `subtract`, `multiply`, `divide`, `modulo`, `int_divide` (for integer division) and `power`. 
 # The function returns the result.
 # ============================================================================
+
+
+
+def calc(a, b, operation="multiply"):
+    if operation == "add":
+        return a + b
+    elif operation == "subtract":
+        return a - b
+    elif operation == "multiply":
+        return a * b
+    elif operation == "divide":
+        return a / b
+    elif operation == "modulo":
+        return a % b
+    elif operation == "int_divide":
+        return a // b
+    elif operation == "power":
+        return a ** b
+
+print(calc(2, 4))
