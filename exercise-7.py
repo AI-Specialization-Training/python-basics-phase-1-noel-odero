97f97c0fdc43bad5271b4cae5a6004dab02b8b1a
Noel Odero <anoel.odero@gmail.com>
2026-03-12 20:20:18 +0200

    Implement number_pattern function to generate a space-separated string of integers from 1 to n with input validation

--- exercise-7.py
+++ exercise-7.py
@@ -19,3 +19,16 @@
 
 # >>>>>>>>>>>>>
 # ============================================================================
+
+def number_pattern(n):
+    if type(n) != int:
+        return "Argument must be an integer value."
+    
+    if n < 1:
+        return "Argument must be an integer greater than 0."
+    
+    result = ""
+    for i in range(1, n + 1):
+        result += str(i) + " "
+    
+    return result.strip()
